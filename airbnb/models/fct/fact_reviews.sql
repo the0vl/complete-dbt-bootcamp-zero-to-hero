3b134e9a98315ec44e4ca79e741bedcb0699ccf9
Theo Vlachos <theo.vlachos123@gmail.com>
2026-04-07 13:37:05 +0300

    macro if statement addition to fact_reviews build

---
+++ airbnb/models/fct/fact_reviews.sql
@@ -0,0 +1,15 @@
+{{ 
+    config(
+        materialised = 'incremental'
+        on_schema_change = 'fail' 
+        )
+}}
+
+with src_reviews as (
+    select * from {{ ref('src_reviews') }}
+)
+SELECT * FROM src_reviews
+WHERE review_text IS NOT NULL
+{% if is_incremental() %}
+    AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
